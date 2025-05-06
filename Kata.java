28dcc204c23623dbe8dd325bb98a13efb2867c9f
therabbi.tim@gmail.com <you@example.com>
2025-05-06 12:03:35 +0100

    Removed unused files

--- Kata.java
+++ Kata.java
@@ -1,21 +1,24 @@
-	public class Kata {
+public class Kata {
 
-		public static int even(int number){
-			int even = number % 2;
-			if ( even == 0 ){
-				System.out.print("This number is an even number.");
+		public static boolean even(int number){
+			boolean even = true; 
+
+			int evenNumber = number % 2;
+
+			if ( evenNumber == 0 ){
+				even = true; 
 			}
 			else {
-				System.out.println("This number is not even number.");
+				even = false;
 				}
 
 			return even;
 		}
-	}
+	
 
 		public static int minus (int num1, int num2){
 			int difference = num1 - num2;
-			int differenceFormatted;
+			int differenceFormatted = 0;
 			if (difference < 0){
 			differenceFormatted = difference * -1;
 			}
@@ -25,10 +28,80 @@
 
 		return differenceFormatted;
 
-
-
-
 		}
 		
+		public static boolean prime (int number){
+
+			boolean prime = true;
+
+		if (number <= 1) prime = false;
+		
+			for(int i = 2; i <= number / 2; i++){
+				if (number % i == 0){
+				prime = false;
+				} else{
+				prime = true;
+				}
+			} 
+		return prime;
+	
+		}
+
+
+		public static float divide (float num1, float num2){
+			float quotient = num1 / num2;
+
+		if ( num2 == 0)
+		quotient = 0;
+
+		return quotient;
+	
+		}
+
+
+
+		public static int factorCounter(int number){
+
+		int factorCounter = 0;	
+		int count = 1;
+
+		while(count <= number){
+			if(number % count == 0)
+			factorCounter ++;
+			count ++;
+
+		}
+
+		return factorCounter;
+
+		}
+
+
+		public static long factorial (long number){
+			long factorial = 1;
+
+		for(int i = 1; i <= number; i++){
+		factorial *= i;
+		//System.out.println(" " + i);
+		}
+		//System.out.printf("%n%d! is %d", number, factorial);
+
+		return factorial;
+	
+		}
+
+	
+		public static int square (int number){
+			int square = number * number;
+
+		return square;
+
+		}
+
+
+
+
+
+}
 		
 		
